3997b4ab23c2fd9655d6e93154d9a1327b01ee30
leekyeongjun <babynov1103@gmail.com>
2022-02-08 14:40:18 +0900

    b

--- C++_folder/Programmers/Hash/phonebook.cpp
+++ C++_folder/Programmers/Hash/phonebook.cpp
@@ -1,16 +1,11 @@
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 bool solution(vector<string> phone_book){
-	bool answer = true;
-	for(int i = 0 ; i<phone_book.size()-1; i++){
-		if(phone_book[i].at(phone_book[i].size()) == phone_book[i+1].at(0)){
-			answer = false;
-			break;
-		}
-	}
+	bool answer = true;	
 
 	return answer;
 }
