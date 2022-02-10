54762f5094ee1e9e9b26469e89859d3cb9f35777
leekyeongjun <babynov1103@gmail.com>
2022-02-10 13:50:09 +0900

    a

--- C++_folder/Coding_Test_Practice/1408.cpp
+++ C++_folder/Coding_Test_Practice/1408.cpp
@@ -1,58 +1,47 @@
-#include <__split_buffer>
 #include <iostream>
 #include <string>
-#include <vector>
-
 
 using namespace std;
 
-string buf;
-int c,l;
+string cur, lef;
 
-int timize(string s){
-	int result;
-	result = stoi(s.substr(0,1))*3600 + stoi(s.substr(3,4))*60 + stoi(s.substr(6,7));
-	return result;
-}
-
-void clockize(int t){
-	int h = t%3600;
-	int m = (t - (h*3600)) % 60;
-	int s = (t - (h*3600)- m*60);
-
-	if(h < 10)
-		cout << "0" << h;
-	else
-		cout << h;
-	
-	cout << ":";
-
-	if(m<10)
-		cout << "0" << m;
-	else
-		cout << m;
-
-	cout << ":";
-
-
-	if(s<10)
-		cout << "0" << s;
-	else
-		cout << s;
-
-	cout << "\n";
-
+void printTime(int t){
+	if(t == 0){
+		cout << "00";
+	}
+	else if(t < 10){
+		cout << "0" << t;
+	}
+	else{
+		cout << t;
+	}
 }
 int main()
 {
-	cin >> buf;
-	c = timize(buf);
+	cin >> cur >> lef;
 
-	cin >> buf;
-	l = timize(buf);
+	int hour = stoi(lef.substr(0,1)) - stoi(cur.substr(0,1));
 
-	clockize(l-c);
+	int minute = stoi(lef.substr(3,4)) - stoi(cur.substr(3,4));
+	
+	int second = stoi(lef.substr(6,7)) - stoi(cur.substr(6,7));
+
+	if(second < 0){
+		second += 60;
+		minute --;
+	}
+			
+	if(minute < 0){
+		minute += 60;
+		hour --;
+	}
+
+	printTime(hour);
+	cout << ":";
+	printTime(minute);
+	cout <<":";
+	printTime(second);
+	cout << '\n';
 
 
 }
-
