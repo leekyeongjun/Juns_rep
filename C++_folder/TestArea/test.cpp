b8e91c21fb9701ab888a2c2a308ae0b46f853913
leekyeongjun <babynov1103@gmail.com>
2022-05-09 11:37:35 +0900

    2920

--- C++_folder/TestArea/test.cpp
+++ C++_folder/TestArea/test.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int n;
-int cnt = 0;
-
-int money[6] = {500,100,50,10,5,1};
-
-void CutBucks(int id){
-//	cout << "Doing.. " << money[id] << " ..." << '\n'; 
-	int cut = money[id];
-	if(n >= cut){
-//		cout << n << " - " << cut*(n/cut) << " ..." << '\n';
-		cnt += n/cut;
-		n -= cut*(n/cut);
-//		cout << "cnt is " << cnt << '\n';
-	}
-}
-
+int arr[9] = {1,2,3,4,5,6,7,8};
+int arr_2[9] = {8,7,6,5,4,3,2,1};
+int cmp[9];
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	int a;
-	cin >> a;
-	n = 1000-a;
-	
-	for(int i = 0; i<6; i++){
-		CutBucks(i);
+	for(int i = 0 ; i < 8; i++){
+		cin >> cmp[i];
 	}
-	cout << cnt << '\n';
+	if(cmp[0] == 1 || cmp[0] == 8){
+		if(cmp[0] == 1){
+			for(int i = 1; i<8; i++){
+				if(arr[i] != cmp[i]) {
+					cout << "mixed" << '\n'; 
+					return 0;
+				}
+			}
+			cout << "ascending" << '\n';
+			return 0;
+		}
+		else if(cmp[0] == 8){
+			for(int i=1; i<8; i++){
+				if(arr_2[i] != cmp[i]){
+					cout << "mixed" << '\n';
+					return 0;
+				}
+			}
+			cout << "descending" << '\n';
+			return 0;
+		}
+	}
+	else{
+		cout << "mixed" << '\n';
+	}
 }
