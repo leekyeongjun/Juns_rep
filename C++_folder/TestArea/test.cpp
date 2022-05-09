a499d2700f11f6537add71ceda12e10cf7770d46
leekyeongjun <babynov1103@gmail.com>
2022-05-09 10:41:43 +0900

    5585

--- C++_folder/TestArea/test.cpp
+++ C++_folder/TestArea/test.cpp
@@ -1,34 +1,34 @@
-#include <functional>
 #include <iostream>
-#include <queue>
-
 using namespace std;
 
-priority_queue<int> q1;
-priority_queue<int, vector<int>, greater<int>> q2;
+int n;
+int cnt = 0;
+
+int money[6] = {500,100,50,10,5,1};
+
+void CutBucks(int id){
+//	cout << "Doing.. " << money[id] << " ..." << '\n'; 
+	int cut = money[id];
+	if(n >= cut){
+//		cout << n << " - " << cut*(n/cut) << " ..." << '\n';
+		cnt += n/cut;
+		n -= cut*(n/cut);
+//		cout << "cnt is " << cnt << '\n';
+	}
+}
 
 int main()
 {
-	int sum = 0;
-	int n;
-	cin >> n;
-
-	for(int i = 0 ; i <n; i++){
-		int a;
-		cin >> a;
-		q2.push(a);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int a;
+	cin >> a;
+	n = 1000-a;
+	
+	for(int i = 0; i<6; i++){
+		CutBucks(i);
 	}
-
-	for(int i=0; i<n; i++){
-		int b;
-		cin >> b;
-		q1.push(b);
-	}
-
-	while(!q1.empty() && !q2.empty()){
-		sum += q1.top()*q2.top();
-		q1.pop();
-		q2.pop();
-	}
-	cout << sum << '\n';
+	cout << cnt << '\n';
 }
