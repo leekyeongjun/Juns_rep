99cccbc932e143db72b2fab1141073e8b3d09594
leekyeongjun <babynov1103@gmail.com>
2022-05-10 10:02:10 +0900

    1085

--- C++_folder/TestArea/test.cpp
+++ C++_folder/TestArea/test.cpp
@@ -1,37 +1,17 @@
 #include <iostream>
+#include <math.h>
+#include <queue>
 using namespace std;
 
-int arr[9] = {1,2,3,4,5,6,7,8};
-int arr_2[9] = {8,7,6,5,4,3,2,1};
-int cmp[9];
+int x,y,w,h;
+priority_queue<int, vector<int>, greater<int>> q;
 int main()
 {
-	for(int i = 0 ; i < 8; i++){
-		cin >> cmp[i];
-	}
-	if(cmp[0] == 1 || cmp[0] == 8){
-		if(cmp[0] == 1){
-			for(int i = 1; i<8; i++){
-				if(arr[i] != cmp[i]) {
-					cout << "mixed" << '\n'; 
-					return 0;
-				}
-			}
-			cout << "ascending" << '\n';
-			return 0;
-		}
-		else if(cmp[0] == 8){
-			for(int i=1; i<8; i++){
-				if(arr_2[i] != cmp[i]){
-					cout << "mixed" << '\n';
-					return 0;
-				}
-			}
-			cout << "descending" << '\n';
-			return 0;
-		}
-	}
-	else{
-		cout << "mixed" << '\n';
-	}
+	cin >> x >> y >> w >> h;
+	q.push(x);
+	q.push(y);
+	q.push(abs(w-x));
+	q.push(abs(h-y));
+
+	cout << q.top() << '\n';
 }
