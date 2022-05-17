7672414e9b4fe033d74c7446247f5adbbd8da021
leekyeongjun <babynov1103@gmail.com>
2022-05-17 10:37:51 +0900

    11724

--- C++_folder/TestArea/test.cpp
+++ C++_folder/TestArea/test.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <queue>
 
@@ -15,6 +16,7 @@ queue<Node> q;
 
 int N, M;
 Node nd[1010];
+int nddt[1010];
 
 int main(){
 	ios::sync_with_stdio(false);
@@ -34,6 +36,8 @@ int main(){
 	for(int i = 0 ; i< N; i++){
 		if(nd[i].data == 0 && !nd[i].edge.empty()){
 			nd[i].data = init_data;
+			nddt[i] = init_data;
+
 			q.push(nd[i]);
 			while(!q.empty()){
 				Node cur = q.front();
@@ -41,7 +45,9 @@ int main(){
 				while(!cur.edge.empty()){
 					if(nd[cur.edge.front()].data == 0){
 						q.push(nd[cur.edge.front()]);
+						
 						nd[cur.edge.front()].data = init_data;
+						nddt[cur.edge.front()] = init_data;
 					}
 					cur.edge.pop();
 				}
@@ -50,7 +56,13 @@ int main(){
 		}
 	}
 
-	cout << init_data << '\n';
+	/*	cout << "init_data : " << init_data << '\n';
+	for(int i = 1; i<= N; i++){
+		cout << i << " : " << nddt[i] << '\n';
+	}
+	*/
 
+	sort(nddt, nddt+N);
+	cout << nddt[N] << '\n';
 }
 
