a68461ae5f4a9c6fbfef07c21e03f48e86d12afd
leekyeongjun <babynov1103@gmail.com>
2022-05-23 10:16:55 +0900

    11726

--- C++_folder/TestArea/test.cpp
+++ C++_folder/TestArea/test.cpp
@@ -2,12 +2,22 @@
 
 using namespace std;
 
+long long f[1001] = {0,1,2};
+
+long long DP(long long n){
+	if(n == 1) return 1;
+	else if(n == 2) return 2;
+	else if(f[n] != 0) return f[n];
+	else{
+		f[n] = (DP(n-1) + DP(n-2)) % 10007;
+		return f[n];
+	}
+}
+
 int main()
 {
-	cout << "	 ,r'\"7" << '\n';
-	cout << "r`-_   ,'  ,/" << '\n';
-	cout << " \\. \". L_r'" << '\n';
-	cout << "   `~\\/" << '\n';
-	cout << "      |" << '\n';
-	cout << "      |" << '\n';
+	long long a;
+	cin >> a;
+	cout << DP(a) << '\n';
+
 }
