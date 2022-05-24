8378f38b3940eddd83a01436e71c3760be606022
leekyeongjun <babynov1103@gmail.com>
2022-05-24 12:12:35 +0900

    11053

--- C++_folder/TestArea/test.cpp
+++ C++_folder/TestArea/test.cpp
@@ -1,23 +1,52 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
+int N;
+int nums[1002];
+int d[1002];
 
-long long f[1001] = {0,1,2};
-
-long long DP(long long n){
-	if(n == 1) return 1;
-	else if(n == 2) return 2;
-	else if(f[n] != 0) return f[n];
+int DP(int n){
+	if(n == 1){
+		return d[1];
+	}
+	else if(d[n] != 0){
+//		cout <<"d[" << n <<"] already exist." << '\n';
+		return d[n];
+	}
 	else{
-		f[n] = (DP(n-1) + DP(n-2)) % 10007;
-		return f[n];
+//		cout <<"d[" << n << "] doesn't exist." << '\n';
+		int cur = nums[n];
+		int s = 1;
+		for(int i=1; i<n; i++){
+			int tmp;
+//			cout << cur << " " << nums[i] << '\n';
+			if(cur > nums[i]){
+//				cout << "finding DP(" << i << ")..." << '\n';
+				tmp = DP(i)+1;
+				s = max(s,tmp);
+			}
+		}
+		d[n] = s;
+		return d[n];
 	}
 }
 
+
 int main()
 {
-	long long a;
-	cin >> a;
-	cout << DP(a) << '\n';
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N;
+	for(int i = 1; i<= N; i++){
+		cin >> nums[i];
+	}
+	d[1] = 1;
+	int S = 1;
+	for(int j=1; j<=N; j++){
+		S = max(S, DP(j));
+	}
+	cout << S << '\n';
 
 }
