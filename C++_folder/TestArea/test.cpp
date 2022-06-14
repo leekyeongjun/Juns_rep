309bf15e9a6786e9cffac24f2c44aecd4ca589ee
leekyeongjun <babynov1103@gmail.com>
2022-06-14 09:45:02 +0900

    2178

--- C++_folder/TestArea/test.cpp
+++ C++_folder/TestArea/test.cpp
@@ -1,36 +1,48 @@
 #include <iostream>
 #include <queue>
+
 using namespace std;
 
-int N;
-priority_queue<int, vector<int>, greater<int>> pq;
+class pos{
+	public:
+	int s,g;
+};
+
+int GRID[101][101];
+int N,M;
+queue<pos> q;
+
 int main(){
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> N;
-
-	for(int i = 0; i<N; i++){int a; cin >> a; pq.push(a);}
-	int cnt = 0;
-	int sum = 0;
-	int ans = 0;
-
-	while(!pq.empty()){
-		sum += pq.top();
-		ans += pq.top();
-		pq.pop();
-		cnt++;
-		/*
-		cout << "current Sum is " << sum << '\n';
-		cout << "current Ans is " << ans << '\n';
-		*/
-		if(pq.empty()) break;
-		else if(cnt == 2){
-			pq.push(sum);
-			sum = 0;
-			cnt = 0;
+	cin >> N >> M;
+	for(int i=0; i<N; i++){
+		for(int j=0; j<M; j++){
+			char a;
+			cin >> a;
+			GRID[i][j] = a -'0';
 		}
 	}
 
-	cout << ans << '\n';
+	//(0,0) start, (N-1,M-1)
+	
+	q.push({0,0});
+	GRID[0][0] = 2;
 
+	int ds[4] = {1,-1,0,0};
+	int dg[4] = {0,0,1,-1};
 
+	while(!q.empty()){
+		pos c = q.front();
+		q.pop();
+		for(int i=0; i<4; i++){
+			pos n = {c.s+ds[i], c.g+dg[i]};
+			if(n.s >= 0 && n.g >= 0 && n.s < N && n.g < M){
+				if(GRID[n.s][n.g] == 1){
+					GRID[n.s][n.g] = GRID[c.s][c.g] + 1;
+					q.push(n);
+				}
+			}
+		}	
+	}
+
+	cout << GRID[N-1][M-1] -1 << '\n';
 }
